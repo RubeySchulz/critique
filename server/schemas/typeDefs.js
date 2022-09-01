2e863b80b489ceec60d5f8701bb6ab5e8d064a57
Ruben <[connor.j.schulz@gmail.com]>
2022-09-01 17:08:11 -0400

    init files

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,18 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        
+    }
+
+    type Mutation {
+        
+    }
+`;
+
+module.exports = typeDefs;
