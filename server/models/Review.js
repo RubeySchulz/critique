4be6879c8b303126c842bd772821d45e3c11f567
Ruben <[connor.j.schulz@gmail.com]>
2022-09-03 06:08:25 -0400

    laid groundwork for mongoDB server with models and typedefs

---
+++ server/models/Review.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const reviewSchema = new Schema(
+    {
+        body: {
+            type: String
+        },
+
+        starRating: {
+            type: Number,
+            required: true,
+            enum: [1, 2, 3, 4, 5]
+        },
+
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+
+        createdAt: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+
+        day: {
+            type: Schema.Types.ObjectId,
+            ref: 'Day'
+        }
+    }
+);
+
+const Review = model('Review', reviewSchema);
+
+module.exports = Review;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,50 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Day {
+        _id: ID
+        date: String
+        item: String
+        reviews: [Review]
+    }
+
+    type Review {
+        _id: ID
+        body: String
+        starRating: Int
+        user: User
+        item: String
+        createdAt: String
+        day: Day
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        reviews: [Review]
+    }
+
     type Auth {
         token: ID!
         user: User
     }
 
     type Query {
-        
+        me: User
+        users: [User]
+        user(username: String!): User
+        days: [Day]
+        day(date: String!): Day
     }
 
     type Mutation {
-        
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addReview(body: String, starRating: Int!, userId: ID!, dayId: ID!): Day
+        addDay(date: String!, item: String!, image: String!): Day
+
     }
 `;
 
